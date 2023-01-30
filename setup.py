f672671f6cf54a0db9426d4eb3e2957e69068843
Alex LaGrassa <lagrassa@cmu.edu>
2023-01-30 14:57:57 -0500

    include_package_data

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='pybullet_arm_course',
       author='Alex LaGrassa, Tiffany Yu',
       author_email='lagrassa@cmu.edu, tiffany2@cmu.edu',
       packages=['pybullet_arm_course', 'pybullet_arm_course.pybullet_tools', 'pybullet_arm_course.pybullet_tools.ikfast'],
-      package_data={"pybullet_arm_course": ['assets/*']}
+      package_data={"pybullet_arm_course": ['assets/*']},
+      include_package_data=True,
      )
 
